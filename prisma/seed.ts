a65849779741288fddf3ab3baaec7d4befe0ef17
May Cheong <maycheongs@gmail.com>
2025-08-22 00:02:51 -0500

    add seeds- 2 lists

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,18 +2,37 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+
+    //USER SEEDS
     const alice = await prisma.user.create({
         data: { name: "Alice", email: "alice@example.com" },
     });
 
+    const bob = await prisma.user.create({
+        data: { name: "Bob", email: "bob@builder.com" },
+    });
+
+    //LIST SEEDS
+
     const list = await prisma.list.create({
         data: { name: "Packing for Trip", type: "PACKING", users: { connect: { id: alice.id } } },
     });
-
     await prisma.item.createMany({
         data: [
             { name: "Toothbrush", listId: list.id, isTask: false },
             { name: "Passport", listId: list.id, isTask: false },
+        ],
+    });
+
+
+    const list2 = await prisma.list.create({
+        data: { name: "Grocery List", type: "TASK", users: { connect: { id: alice.id } } },
+    });
+    await prisma.item.createMany({
+        data: [
+            { name: "Milk", listId: list2.id, isTask: false },
+            { name: "Eggs", listId: list2.id, isTask: false },
+            { name: "Bread", listId: list2.id, isTask: false },
         ],
     });
 }
