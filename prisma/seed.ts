394a8024d9d4d95dbae731d008152b8baa4f03f3
May Cheong <maycheongs@gmail.com>
2025-08-22 23:05:44 -0500

    add more varied seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,6 +14,7 @@ async function main() {
 
     //LIST SEEDS
 
+    //Packing list with no categories, item is lastMinute
     const list = await prisma.list.create({
         data: { name: "Packing for Trip", type: "PACKING", users: { connect: { id: alice.id } } },
     });
@@ -21,18 +22,28 @@ async function main() {
         data: [
             { name: "Toothbrush", listId: list.id, isTask: false },
             { name: "Passport", listId: list.id, isTask: false },
+            { name: "Sunglasses (last minute)", listId: list.id, isTask: false, lastMinute: true },
         ],
     });
 
-
-    const list2 = await prisma.list.create({
+    //Grocery list with 2 categories, one item is checked, one item is lastMinute, one item has no category
+    const groceryList = await prisma.list.create({
         data: { name: "Grocery List", type: "TASK", users: { connect: { id: alice.id } } },
+    });
+    const dairyCategory = await prisma.category.create({
+        data: { name: "Dairy", listId: groceryList.id },
+    });
+
+    const fruitsCategory = await prisma.category.create({
+        data: { name: "Fruits", listId: groceryList.id },
     });
     await prisma.item.createMany({
         data: [
-            { name: "Milk", listId: list2.id, isTask: false },
-            { name: "Eggs", listId: list2.id, isTask: false },
-            { name: "Bread", listId: list2.id, isTask: false },
+            { name: "Milk", listId: groceryList.id, categoryId: dairyCategory.id },
+            { name: "Cheese", listId: groceryList.id, categoryId: dairyCategory.id, checked: true },
+            { name: "Apples", listId: groceryList.id, categoryId: fruitsCategory.id },
+            { name: "Bananas (last minute)", listId: groceryList.id, lastMinute: true },
+            { name: "Bread", listId: groceryList.id }, // no category
         ],
     });
 }
