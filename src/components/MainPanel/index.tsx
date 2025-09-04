31df56773692bd695ecbc1fae3cb2e8bfa35e737
May Cheong <maycheongs@gmail.com>
2025-09-04 15:34:06 -0500

    split components into folder by hierarchy

---
+++ src/components/MainPanel/index.tsx
@@ -0,0 +1,56 @@
+//components/MainPanel
+
+'use client';
+
+import { Box, Heading, VStack, Checkbox, Spinner, Center, Button } from '@chakra-ui/react';
+import { useListContext } from '../ListContext';
+import { useGetListQuery } from '@/graphql/codegen';
+import ItemsContainer from './ItemsContainer';
+import ListHeader from './ListHeader';
+
+
+export default function MainPanel() {
+    const { selectedListId } = useListContext();
+
+    // Skip query if no list is selected
+    const { data, loading, error } = useGetListQuery({
+        variables: { id: selectedListId! }, // non-null because we skip below
+        skip: !selectedListId,
+    });
+
+    console.log('MainPanel data:', data, 'loading:', loading, 'error:', error, 'selectedListId:', selectedListId);
+
+    if (!selectedListId) {
+        return (
+            <Center height="100%">
+                <Button colorScheme="blue">Create List</Button>
+            </Center>
+        );
+    }
+
+    if (loading) {
+        return (
+            <Center height="100%">
+                <Spinner size="xl" />
+            </Center>
+        );
+    }
+
+    if (error) return <Box>Error loading list: {error.message}</Box>;
+
+    const list = data?.list;
+    if (!list) return <Box>List not found</Box>;
+
+    // Sort items: unchecked first, then checked
+    // const sortedItems = [...list.items].sort((a, b) => Number(a.checked) - Number(b.checked));
+
+
+
+
+    return (
+        <Box as="main" fontSize={14}>
+            <ListHeader list={list} />
+            <ItemsContainer items={list.items} categories={list.categories || []} />
+        </Box>
+    );
+}
