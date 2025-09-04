af8c3d84ca4d6e7dc1c5eced2fa876f5f9e940e1
May Cheong <maycheongs@gmail.com>
2025-09-04 15:34:54 -0500

    organize items in categories in itemscontainer and categorysection

---
+++ src/components/MainPanel/ItemsContainer/CategorySection.tsx
@@ -0,0 +1,46 @@
+//components/MainPanel/ItemsContainer/CategorySection.tsx
+'use client';
+import { Box, VStack, Badge } from '@chakra-ui/react';
+import ListItem from './ListItem'
+import { Item } from '.';
+
+type CategorySectionProps = {
+    categoryKey?: string | null;
+    items: Item[]
+    isLastMinute?: boolean;
+    isChecked?: boolean;
+    color: string;
+};
+
+function CategorySection({ categoryKey, items, isChecked, isLastMinute, color }: CategorySectionProps) {
+
+    console.log('ischecked', isChecked, 'items', items);
+
+    const [categoryId, categoryName] = categoryKey ? categoryKey.split('-') : [null, null];
+
+
+    return (
+        <Box py={1} >
+            {(categoryKey || isChecked || isLastMinute) ?
+                <Badge mb={1} size='md' bg={`${color}.100`} fontWeight='400'>
+                    {categoryName || (isLastMinute ? 'Last Minute' : 'Completed')}
+                </Badge> : ''}
+            <VStack as="ul" gap={1} align="stretch" className="group">
+                {items.map(item => (
+                    <ListItem
+                        key={item.id}
+                        item={item}
+                        onToggleCheck={(item) => { console.log('toggle check', item) }}
+                        onToggleLastMinute={(item) => { console.log('toggle last minute', item) }}
+                        onSetCategory={(cat) => console.log('Set category', cat)}
+                        categories={[{ id: 1, name: 'Cat1' }, { id: 2, name: 'Cat2' }]}
+                        color={color}
+                    />
+                ))}
+            </VStack>
+        </Box>
+    )
+
+}
+
+export default CategorySection;
---
+++ src/components/MainPanel/ItemsContainer/index.tsx
@@ -0,0 +1,111 @@
+//components/MainPanel/ItemsContainer
+
+import { GetListQuery } from '@/graphql/codegen';
+import { Box, VStack } from '@chakra-ui/react';
+import CategorySection from './CategorySection';
+
+export type Item = NonNullable<GetListQuery['list']>['items'][0] & { color?: string; };
+
+interface OrganizedItems {
+    categorized: Record<string, Item[]>;
+    uncategorized: Item[];
+    lastMinute: Item[];
+    checked: Item[];
+}
+
+interface ItemsContainerProps {
+    items: Item[],
+    categories?: NonNullable<GetListQuery['list']>['categories']
+};
+
+
+const categoryColors = ['blue', 'green', 'purple', 'red', 'orange', 'pink', 'yellow', 'teal', 'cyan', 'gray'];
+
+
+const ItemsContainer = ({ items, categories }: ItemsContainerProps) => {
+
+
+    const categoryColorMap: Record<number, string> = {};
+    (categories || []).forEach((cat, idx) => {
+        categoryColorMap[cat.id] = categoryColors[idx % categoryColors.length];
+    });
+
+    const { categorized, uncategorized, lastMinute, checked } = organizeItems(items);
+
+    function organizeItems(items: Item[]): OrganizedItems {
+        const categorized: Record<string, Item[]> = {};
+        const uncategorized: Item[] = [];
+        const lastMinute: Item[] = [];
+        const checked: Item[] = [];
+
+        items.forEach(obj => {
+            let item = { ...obj }; // Create a mutable copy of the item
+
+            if (item.category) {
+                item.color = categoryColorMap[item.category.id]
+            }
+
+            if (item.checked || (item.lastMinute && item.checked)) {
+                checked.push(item);
+            } else if (item.lastMinute) {
+                lastMinute.push(item);
+            } else if (item.category) {
+                const categoryKey = `${item.category.id}-${item.category.name}`
+                if (!categorized[categoryKey]) {
+                    categorized[categoryKey] = [];
+                }
+                categorized[categoryKey].push(item);
+            } else {
+                uncategorized.push(item);
+            }
+        });
+
+
+        return { categorized, uncategorized, lastMinute, checked };
+    };
+    return (
+        <Box as="section" flex={1} overflowY="auto" py={2} px={5} bg='gray.200'>
+            <VStack gap={0} align="stretch">
+                {/* Uncategorized */}
+                {uncategorized.length ?
+                    <CategorySection
+                        categoryKey={null}
+                        items={uncategorized}
+                        color='gray'
+
+                    /> : ''}
+
+                {/* Categories */}
+                {Object.keys(categorized).length ?
+                    Object.entries(categorized).map(([categoryKey, items], index) => (
+                        <CategorySection
+                            categoryKey={categoryKey}
+                            items={items}
+                            color={categoryColorMap[Number(categoryKey.split('-')[0])]}
+                        />
+                    )) : ''}
+
+
+
+                {/* Last Minute */}
+                {lastMinute.length ?
+                    <CategorySection
+                        items={lastMinute}
+                        isLastMinute={true}
+                        color='gray'
+                    /> : ''}
+
+                {/* Checked Items */}
+                {checked.length ? (
+                    <CategorySection
+                        items={checked}
+                        isChecked={true}
+                        color='gray'
+                    />
+                ) : ''}
+            </VStack>
+        </Box>
+    )
+}
+
+export default ItemsContainer;
