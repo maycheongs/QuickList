7f2af5b1a5a34d1d6ea392ff33d3c30e301baa92
May Cheong <maycheongs@gmail.com>
2025-09-19 21:06:27 -0500

    added add list icon on sidebar

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,8 +1,9 @@
 //src/components/SidePanel.tsx
 'use client';
 
-import { VStack, Heading, Box } from '@chakra-ui/react';
+import { VStack, Heading, Box, HStack, Separator, Button, Text, IconButton } from '@chakra-ui/react';
 import { useListContext } from '../contexts/ListContext';
+import { ListPlus } from 'lucide-react'
 
 
 export default function SidePanel() {
@@ -11,22 +12,41 @@ export default function SidePanel() {
     if (error) return <div>Error: {error.message}</div>;
 
     return (
-        <VStack as="nav" gap={2} align="stretch" p={4} height='100%' overflowY="auto">
-            <Heading size="md">My Lists</Heading>
-            {lists.length === 0 && <Box>No lists. Create one!</Box>}
+        <VStack as="nav" gap={1} align="stretch" p={4} height='100%' overflowY="auto" fontSize={'sm'} minWidth={150}>
+            <HStack gap={2} justify='space-between' pr={3} pb={2}>
+                <Heading size="lg">My Lists</Heading>
+                <IconButton display={{ base: "none", sm: "block" }} variant='ghost' aria-label="add-list"><ListPlus /></IconButton>
+            </HStack>
+            <Box mt={2} display={{ base: "block", sm: "none" }}>
+                <Separator />
+                <Box px={2} py={2}><HStack justify='space-between'><Text fontWeight='semibold'>Add Item</Text> <ListPlus /></HStack></Box>
+                <Separator />
+            </Box>
             {lists.map((list) => (
                 <Box
                     key={list.id}
                     p={2}
-                    border="1px solid lightgray"
+                    pl={3}
                     borderRadius="md"
                     cursor="pointer"
-                    bg={selectedListId === list.id ? 'gray.100' : 'white'}
+                    data-selected={selectedListId === list.id ? "" : undefined}
+                    _selected={{
+                        bg: "blue.50",
+                        color: "blue.700",
+                        fontWeight: "semibold",
+                        _hover: {
+                            bg: "blue.50",
+                        },
+                    }}
                     onClick={() => setSelectedListId(list.id)}
+                    _hover={{
+                        bg: "gray.100"
+                    }}
                 >
                     {list.name}
                 </Box>
             ))}
+
         </VStack>
     );
 }
